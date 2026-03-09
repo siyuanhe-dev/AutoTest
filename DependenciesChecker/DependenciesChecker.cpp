86c8ffe253e1928b97c85af368d418787b2660c5
Siyuan He <regent.molder_4c@icloud.com>
2026-03-09 22:34:33 +0800

    Format newly writed codes

--- DependenciesChecker/DependenciesChecker.cpp
+++ DependenciesChecker/DependenciesChecker.cpp
@@ -7,46 +7,60 @@ static bool IsProcessElevated()
 {
     BOOL bElevated = FALSE;
     HANDLE token = NULL;
-    if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &token)) {
+
+    if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &token))
+    {
         TOKEN_ELEVATION elevation;
         DWORD retLen = 0;
-        if (GetTokenInformation(token, TokenElevation, &elevation, sizeof(elevation), &retLen)) {
+
+        if (GetTokenInformation(token, TokenElevation, &elevation, sizeof(elevation), &retLen))
+        {
             bElevated = elevation.TokenIsElevated;
         }
+
         CloseHandle(token);
     }
+
     return bElevated == TRUE;
 }
 
 static bool FindExecutableOnPath(LPCWSTR exeName, std::wstring &foundPath)
 {
-    wchar_t buf[MAX_PATH] = {0};
+    wchar_t buf[MAX_PATH] = { 0 };
     DWORD len = SearchPathW(NULL, exeName, NULL, MAX_PATH, buf, NULL);
-    if (len > 0 && len < MAX_PATH) {
+
+    if (len > 0 && len < MAX_PATH)
+    {
         foundPath = buf;
         return true;
     }
+
     return false;
 }
 
 static HRESULT TryRelaunchElevated()
 {
-    wchar_t exePath[MAX_PATH] = {0};
-    if (GetModuleFileNameW(NULL, exePath, MAX_PATH) == 0) {
+    wchar_t exePath[MAX_PATH] = { 0 };
+
+    if (GetModuleFileNameW(NULL, exePath, MAX_PATH) == 0)
+    {
         return HRESULT_FROM_WIN32(GetLastError());
     }
 
     HINSTANCE result = ShellExecuteW(NULL, L"runas", exePath, NULL, NULL, SW_SHOWNORMAL);
-    if ((INT_PTR)result > 32) {
-        return S_OK; // launched elevated process
+
+    if ((INT_PTR)result > 32)
+    {
+        return S_OK;
     }
-    // User probably cancelled or failed
+
     return HRESULT_FROM_WIN32(ERROR_CANCELLED);
 }
 
 extern "C" __declspec(dllexport) HRESULT CheckAndFixDependencies()
 {
-    bool hasWpr = false, hasWpa = false;
+    bool hasWpr = false;
+    bool hasWpa = false;
     std::wstring path;
 
     hasWpr = FindExecutableOnPath(L"wpr.exe", path);
@@ -54,31 +68,40 @@ extern "C" __declspec(dllexport) HRESULT CheckAndFixDependencies()
 
     bool elevated = IsProcessElevated();
 
-    if (hasWpr && hasWpa && elevated) {
+    if (hasWpr && hasWpa && elevated)
+    {
         return S_OK;
     }
 
-    // If not elevated, try to relaunch elevated. This will prompt UAC.
-    if (!elevated) {
+    if (!elevated)
+    {
         HRESULT hr = TryRelaunchElevated();
-        if (SUCCEEDED(hr)) {
-            // We launched an elevated instance; caller should exit and let elevated instance continue
+
+        if (SUCCEEDED(hr))
+        {
             return S_FALSE;
         }
-        // If relaunch failed or user cancelled, fall through to error handling
     }
 
-    // If missing WPR/WPA, show an error message and return failure
-    if (!hasWpr || !hasWpa) {
+    if (!hasWpr || !hasWpa)
+    {
         std::wstring msg = L"Required dependencies not found:\n";
-        if (!hasWpr) msg += L" - wpr.exe (Windows Performance Recorder)\n";
-        if (!hasWpa) msg += L" - wpa.exe (Windows Performance Analyzer)\n\n";
+
+        if (!hasWpr)
+        {
+            msg += L" - wpr.exe (Windows Performance Recorder)\n";
+        }
+
+        if (!hasWpa)
+        {
+            msg += L" - wpa.exe (Windows Performance Analyzer)\n\n";
+        }
+
         msg += L"Please install the Windows Performance Toolkit (part of the Windows ADK) and try again.";
         MessageBoxW(NULL, msg.c_str(), L"Dependencies Missing", MB_ICONERROR | MB_OK);
         return HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND);
     }
 
-    // If we reach here, elevation was required but relaunch failed
     MessageBoxW(NULL, L"Administrator privileges are required. Please restart the application as an administrator.", L"Elevation Required", MB_ICONERROR | MB_OK);
     return HRESULT_FROM_WIN32(ERROR_ELEVATION_REQUIRED);
 }
--- DependenciesChecker/dllmain.cpp
+++ DependenciesChecker/dllmain.cpp
@@ -1,31 +1,30 @@
-﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
-#include "pch.h"
+﻿#include "pch.h"
 #include "DependenciesChecker.h"
 
 static DWORD WINAPI DependencyThreadProc(LPVOID)
 {
-    // Run dependency check on a background thread to avoid loader lock issues
     CheckAndFixDependencies();
     return 0;
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule,
+                      DWORD ul_reason_for_call,
+                      LPVOID lpReserved)
 {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
+    {
         DisableThreadLibraryCalls(hModule);
-        // Start background thread to check dependencies
         CreateThread(NULL, 0, DependencyThreadProc, NULL, 0, NULL);
         break;
+    }
+
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
         break;
     }
+
     return TRUE;
 }
-
